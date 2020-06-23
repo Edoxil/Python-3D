268a7e7b7195f1cc9ccbd9ce53282ac1533abde4
Edoxil <61228578+Edoxil@users.noreply.github.com>
2020-06-23 14:34:37 +0300

    Create main menu
    Created main menu UI and scripts for buttons

---
+++ Python 3D/Assets/Scripts/UI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UI : MonoBehaviour
+{
+    public Texture2D cursor;
+
+    void Awake()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+    public void BtnStart_Click()
+    {
+       
+        SceneManager.LoadScene(1);
+    }
+
+    public void BtnExit_Click()
+    {
+        Application.Quit(0);
+    }
+}
